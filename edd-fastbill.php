697dfe2f3ae0458577d322845bc2c4d0fad3e8d4
Markus Drubba <kontakt@markusdrubba.de>
2017-02-04 14:51:35 +0100

    prepare for release

--- edd-fastbill.php
+++ edd-fastbill.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name: Easy Digital Downloads - FastBill Integration
-Plugin URI: https://easydigitaldownloads.com/extensions/fastbill-integration/?ref=2325
-Description: Integrates <a href="https://easydigitaldownloads.com/" target="_blank">Easy Digital Downloads</a> with the <a href="http://www.fastbill.com" target="_blank">FastBill - fast money</a> accounting software. 
+Plugin URI: https://wordpress.org/plugins/edd-fastbill/
+Description: Integrates <a href="https://easydigitaldownloads.com/?ref=2325" target="_blank">Easy Digital Downloads</a> with the <a href="http://www.fastbill.com" target="_blank">FastBill - fast money</a> accounting software.
 Author: Markus Drubba
 Author URI: http://markusdrubba.de
-Version: 1.4.4
+Version: 1.5.0
 Text Domain: edd-fastbill
 Domain Path: /languages
 
